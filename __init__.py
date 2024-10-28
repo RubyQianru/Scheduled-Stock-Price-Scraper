caa82e9801aa2f6cc25053b079c4904e109be97f
RubyQianru <qz2432@nyu.edu>
2024-10-28 19:35:53 +0000

    Add twitter scraper

--- __init__.py
+++ __init__.py
@@ -2,5 +2,4 @@ import utils as u
 
 if __name__ == "__main__":
     # insert_time_series_data()
-    u.insert_market_data()
     u.insert_twitter_data()
--- utils.py
+++ utils.py
@@ -2,11 +2,14 @@ import http.client
 import json
 from datetime import datetime
 from pymongo import MongoClient
-from config import MONGO_URI, DB_NAME, USERNAME, PASSWORD, API_KEY
+from config import MONGO_URI, CRYPTO_DB, USERNAME, PASSWORD, API_KEY, TWITTER_DB
 
 client = MongoClient(MONGO_URI, username=USERNAME, password=PASSWORD)
-db = client[DB_NAME]
-collection = db["crypto"]
+crypto_db = client[CRYPTO_DB]
+crypto_collection = crypto_db["crypto"]
+
+twitter_db = client[TWITTER_DB]
+twitter_collection = twitter_db["twitter"]
 
 def insert_market_data(symbol="BTC"):
   try:
@@ -46,9 +49,70 @@ def insert_market_data(symbol="BTC"):
       }
       
       print(f"Inserting data: {insert_data}")
-      collection.insert_one(insert_data)
+      crypto_collection.insert_one(insert_data)
     else:
         print("No data received for the symbol")
     
   except Exception as e:
     print(f"Error in coin market API: {str(e)}")
+
+
+def insert_twitter_data(query="bitcoin", symbol="BTC"):
+
+  try:
+    # API call
+    conn = http.client.HTTPSConnection("twitter-api45.p.rapidapi.com")
+
+    headers = {
+        'x-rapidapi-key': API_KEY,
+        'x-rapidapi-host': "twitter-api45.p.rapidapi.com"
+    }
+
+    conn.request("GET", f"/search.php?query={query}&search_type=Top", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read().decode("utf-8")
+    parsed_data = json.loads(data)['timeline']
+
+    print(parsed_data)
+    insert_data = []
+    if parsed_data and len(parsed_data) > 0:
+      for item in parsed_data:
+        tweet_id = item['tweet_id']
+        screen_name = item['screen_name']
+        bookmarks = item['bookmarks']
+        favorites = item['favorites']
+        created_at = item["created_at"]
+        text = item['text']
+        lang = item['lang']
+        quotes = item['quotes']
+        replies = item['replies']
+        retweets = item['retweets']
+        current_time = datetime.now()
+
+        insert_data.append({
+          "symbol": symbol,
+          "name": query,
+          'tweet_id': tweet_id,
+          'screen_name': screen_name,
+          'bookmarks': bookmarks,
+          'favorites': favorites,
+          'created_at': created_at,
+          'text': text,
+          'lang': lang,
+          'quotes': quotes,
+          'replies': replies,
+          'retweets': retweets,
+          "time": current_time,
+        })
+      
+      print(f"Inserting data: {insert_data}")
+      twitter_collection.insert_many(insert_data)
+    else:
+        print("No data received for the symbol")
+    
+  except Exception as e:
+    print(f"Error in twitter API: {str(e)}")
+
+
+
